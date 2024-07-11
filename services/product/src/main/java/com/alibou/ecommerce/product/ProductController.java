ba7ce59f0ae5fa1402bcc62857b979ab6ae3be42
Piyush kumar <kmrpiyush95@gmail.com.com>
2024-07-11 11:18:15 +0530

    test

--- services/product/src/main/java/com/alibou/ecommerce/product/ProductController.java
+++ services/product/src/main/java/com/alibou/ecommerce/product/ProductController.java
@@ -3,12 +3,7 @@ package com.alibou.ecommerce.product;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,6 +21,12 @@ public class ProductController {
         return ResponseEntity.ok(service.createProduct(request));
     }
 
+    @PutMapping
+    public ResponseEntity<Integer> updateProduct(
+            @RequestBody @Valid ProductRequest request
+    ) {
+        return ResponseEntity.ok(service.createProduct(request));
+    }
     @PostMapping("/purchase")
     public ResponseEntity<List<ProductPurchaseResponse>> purchaseProducts(
             @RequestBody List<ProductPurchaseRequest> request
